6756717a0e93261f5af4c6c8fe07c83d07c409c4
Matthias <mat.boen@gmail.com>
2018-12-03 20:25:59 +0100

    add tool.form (placement and naming of all planes, buttons and fields)

---
+++ src/main/java/tool.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class tool {
+    private JTabbedPane tabbedPane1;
+    private JTextPane lendedPane;
+    private JTextField lenderFieldLent;
+    private JTextField itemFieldlent;
+    private JButton lentButton;
+    private JTextField itemFieldReturn;
+    private JButton returnButton;
+    private JPanel lenderPlane;
+    private JPanel lentPlane;
+    private JPanel overviewPlane;
+    private JPanel returnPlane;
+    private JTextField lenderNameField;
+    private JTextField lenderAddressField;
+    private JButton makeLenderButton;
+    private JPasswordField adminPasswordField;
+    private JTextField addTypeTextField;
+    private JTextField removeTypeTextField;
+    private JComboBox addItemComboBox;
+    private JButton addTypeButton;
+    private JButton addItemButton;
+    private JButton removeItemButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
